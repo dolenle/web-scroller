0c2d013f892e7a9738ec952f2d76b42dc3857e62
Dolen Le <le2@cooper.edu>
2016-04-09 01:20:41 -0400

    enhanced server admin page functionality, add feeds and change ssid

--- ESP_NewsRSS/ESP_NewsRSS.ino
+++ ESP_NewsRSS/ESP_NewsRSS.ino
@@ -1,5 +1,6 @@
 /**
  * ESP8266 RSS Feed
+ * Dolen Le
  */
 
 #include <Arduino.h>
@@ -8,46 +9,59 @@
 #include <ESP8266WebServer.h>
 
 #define MAX_FEEDS 5
-#define MAX_LINES 20
+#define MAX_LINES 25
+#define MAX_LENGTH 192
+#define URL_LENGTH 96
 #define RSS_INTERVAL 600000
-#define TIME_SET_INTERVAL 3600000
-#define DISPLAY_INTERVAL 10000
+#define CLOCK_INTERVAL 3600000
+#define DISPLAY_INTERVAL 12000
 
-char headlines[MAX_LINES][128];
+char headlines[MAX_LINES][MAX_LENGTH];
 int line;
 unsigned long lastDisp;
 unsigned long lastUpdate = -RSS_INTERVAL;
-unsigned long lastTimeSet = -TIME_SET_INTERVAL;
+unsigned long lastTimeSet = -CLOCK_INTERVAL;
 char* monthTable PROGMEM = "Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec";
+char* redirHeader PROGMEM = "HTTP/1.1 301 OK\r\nLocation: /\r\nCache-Control: no-cache\r\n\r\n";
 
 uint8_t numFeeds = 2;
-char feedURLs[MAX_FEEDS][96] = {
+uint8_t feedSkip[MAX_FEEDS] = {1, 2};
+uint8_t feedLines[MAX_FEEDS] = {10, 1};
+char feedURLs[MAX_FEEDS][URL_LENGTH] = {
   "http://www.newslookup.com/rss/wires/newslookup-wires.rss",
   "http://rss.accuweather.com/rss/liveweather_rss.asp?locCode=10003"
 };
 
-uint8_t feedSkip[MAX_FEEDS] = {1, 2};
-uint8_t feedLines[MAX_FEEDS] = {10, 1};
+//WiFi info
+char ap_ssid[32] = "esp8266";
+char st_ssid[32] = "esports-league";
+char st_pass[32] = "cuesports";
 
-ESP8266WebServer server(80);
+ESP8266WebServer server(80); //http server on port 80
 
 void setup() {
   Serial.begin(115200);
   Serial1.begin(115200);
-  Serial.println();
   
-  WiFi.begin("esports-league", "cuesports");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println(WiFi.localIP());
+  WiFi.begin(st_ssid, st_pass);
+  WiFi.softAP(ap_ssid);
+  
   server.on("/", pageHandler);
   server.on("/msg", msgHandler);
   server.on("/feed", feedHandler);
-  server.on("/time", updateTime);
-  server.on("/rss", refreshHandler);
+  server.on("/clock", updateTime);
+  server.on("/reset", resetHandler);
+  server.on("/wifi", wifiHandler);
+  server.on("/get", loadMsg);
   server.begin();
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    server.handleClient();
+    Serial.print(".");
+  }
+  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.softAPIP());
 }
 
 void loop() {
@@ -57,7 +71,7 @@ void loop() {
     updateFeeds();
     loadMsg();
     lastUpdate = now;
-  } else if(now-lastTimeSet > TIME_SET_INTERVAL) {
+  } else if(now-lastTimeSet > CLOCK_INTERVAL) {
     updateTime();
     lastTimeSet = now;
     Serial.println("TimeSet");
@@ -72,10 +86,11 @@ void loop() {
   }
 }
 
+//replace reference string with character
 void fixCharRef(char* reference, char sub) {
   char* schar;
   int refLen = strlen(reference);
-  for(int i=0; i<10; i++) {
+  for(int i = 0; i < line; i++) {
     while (schar = strstr(headlines[i], reference)) {
       *schar = sub;
       for (schar += refLen; *schar != 0; schar++) {
@@ -86,6 +101,7 @@ void fixCharRef(char* reference, char sub) {
   }
 }
 
+//load up to maxLines titles from the feed, skipping at beginning
 int loadFeed(char* url, int skip, int maxLines) {
   int src = 0; //source index (from buff)
   int dest = 0; //destination index
@@ -155,7 +171,7 @@ int loadFeed(char* url, int skip, int maxLines) {
     Serial.println("HTTP close");
   
   } else {
-    Serial.printf("HTTP ERROR %d\n", httpCode);
+    Serial.printf("HTTP error %d\n", httpCode);
   }
   http.end();
   fixCharRef("&apos;", '\'');
@@ -168,22 +184,22 @@ int loadFeed(char* url, int skip, int maxLines) {
 void updateTime() {
   WiFiClient client;
   while(!client.connect("google.com", 80)) {
-    Serial.println("google connect error");
+    Serial.println("Time connect error");
   }
   client.print("HEAD / HTTP/1.1\r\n\r\n");
   while(!client.available()) {
      yield();
   }
   while(client.available()) { 
-    if (client.read() == 't' && client.read() == 'e' && client.read() == ':') {
+    if (client.read() == 't' && client.read() == 'e' && client.read() == ':') { //Date:
       client.read();
       char dateStr[30];
       dateStr[29] = 0;
-      client.readBytesUntil('G', dateStr, 30);
+      client.readBytesUntil('G', dateStr, 29);
       char* c = dateStr;
-      while(c=strchr(c+1, ' ')) *c=0;
+      while(c=strchr(c+1, ' ')) *c=0; //tokenize space
       c = dateStr+17;
-      while(c=strchr(c+1, ':')) *c=0;
+      while(c=strchr(c+1, ':')) *c=0; //tokenize colon
       Serial1.write('\005');
       Serial1.write(dateStr+5, 3); //day
       Serial1.printf("%02d", (strstr(monthTable, dateStr+8)-monthTable)/4+1); //month
@@ -191,6 +207,7 @@ void updateTime() {
       client.stop();
     }
   }
+  server.sendContent(redirHeader);
 }
 
 void loadMsg() {
@@ -201,16 +218,17 @@ void loadMsg() {
   if(httpCode == HTTP_CODE_OK && line < MAX_LINES) {
     String payload = http.getString();
     Serial.println(payload);
-    payload.substring(2).toCharArray(headlines[line], payload.length());
+    payload.toCharArray(headlines[line++], payload.length());
   } else {
     Serial.println("Error");
   }
   http.end();
+  server.sendContent(redirHeader);
 }
 
 int updateFeeds() {
   line = 0;
-  for(int i=0; i<numFeeds && line<MAX_LINES; i++) {
+  for(int i = 0; i < numFeeds && line < MAX_LINES; i++) {
     loadFeed(feedURLs[i], feedSkip[i], feedLines[i]);
   }
   Serial.print(line);
@@ -224,29 +242,41 @@ int updateFeeds() {
   return line;
 }
 
+//Main server page
 void pageHandler() {
-  String content = "<html><body><h1>web-scroller</h1><form action='/msg' method='POST'>Force Msg:<input type='text' name='msg'> <input type='submit'></form><form action='/feed' method='POST'>RSS Feeds:<br>";
+  String content = "<html><body><h1>ESP8266 web-scroller</h1><form action='/msg' method='POST'>Force Msg:<input type='text' name='msg' size=90> <input type='submit'></form>WiFi Settings:<form action='/wifi' method='POST'>SSID:<input type='text' name='st_ssid' value='";
+  content += String(st_ssid)+"'> PASS:<input type='password' name='st_pass' placeholder='*****'> AP SSID:<input type='text' name='ap_ssid' value='"+String(ap_ssid)+"'> <input type='submit'></form><form action='/feed' method='POST'>Loaded: (";
+  content += (millis() - lastUpdate)/1000;
+  content += " sec ago)<ol>";
   int i;
-  for(i=0; (numFeeds<MAX_FEEDS && i<=numFeeds) || (numFeeds==MAX_FEEDS && i<numFeeds); i++) {
-    content += "URL:<input type='text' name='url"+String(i)+"' size='80' value='";
-    content += feedURLs[i];
-    content += "'> Skip:<input type='text' name='skip"+String(i)+"' size='3' value='"+String(feedSkip[i])+"'> Max:<input type='text' name='max"+String(i)+"' size='3' value='"+String(feedLines[i])+"'><br>";
+  for (int i = 0; i < line; i++) {
+    content += "<li>";
+    content += headlines[i];
+    content += "</li>";
   }
-  content += "<input type='submit'></form><a href='/time'>Set Clock</a> <a href='/rss'>Update RSS</a></body></html>";
+  content += "</ol>Feeds:<br>";
+  for(i = 0; (numFeeds < MAX_FEEDS && i <= numFeeds) || (numFeeds == MAX_FEEDS && i < numFeeds); i++) {
+    content += "URL:<input type='text' name='url"+String(i)+"' size=80 value='";
+    content += feedURLs[i];
+    content += "'> Skip:<input type='text' name='skip"+String(i)+"' size=3 value='"+String(feedSkip[i])+"'> Max:<input type='text' name='max"+String(i)+"' size=3 value='"+String(feedLines[i])+"'><br>";
+  }
+  content += "<input type='submit'></form><a href='/clock'>Update Clock</a><br><a href='/reset'>Reset Display</a><br><a href='/get'>Get Message</a></body></html>";
   server.send(200, "text/html", content);
 }
 
+//Immediately and temporarily write a message to the display
 void msgHandler() {
-  if (server.hasArg("msg")){
+  if (server.hasArg("msg")) {
     Serial1.println(server.arg("msg"));
   }
-  server.sendContent("HTTP/1.1 301 OK\r\nLocation: /\r\nCache-Control: no-cache\r\n\r\n");
+  server.sendContent(redirHeader);
 }
 
+//Add/remove feed URLs
 void feedHandler() {
   char urlID[5]="url", skipID[6]="skip", maxID[5]="max";
   uint8_t i;
-  for(i=0; i<MAX_FEEDS; i++) {
+  for(i = 0; i < MAX_FEEDS; i++) {
     strcpy(urlID+3, String(i).c_str());
     strcpy(skipID+4, String(i).c_str());
     strcpy(maxID+3, String(i).c_str());
@@ -260,12 +290,36 @@ void feedHandler() {
     }
   }
   numFeeds = i;
-  server.sendContent("HTTP/1.1 301 OK\r\nLocation: /\r\nCache-Control: no-cache\r\n\r\n");
+  updateFeeds();
+  server.sendContent(redirHeader);
 }
 
-void refreshHandler() {
-  String num = String(updateFeeds());
-  num.concat(" titles loaded");
-  server.send(200, "text/html", num);
+//Reset the display driver
+void resetHandler() {
+  pinMode(0, OUTPUT);
+  delay(10);
+  pinMode(0, INPUT);
+  server.sendContent(redirHeader);
+}
+
+//Update WiFi Settings
+void wifiHandler() {
+  if (server.hasArg("st_ssid") && server.hasArg("st_pass") && server.hasArg("ap_ssid")) {
+    server.arg("st_ssid").toCharArray(st_ssid, sizeof(st_ssid));
+    if(server.arg("st_pass").length()) {
+      server.arg("st_pass").toCharArray(st_pass, sizeof(st_pass));
+    }
+    server.arg("ap_ssid").toCharArray(ap_ssid, sizeof(ap_ssid));
+    WiFi.disconnect();
+    WiFi.begin(st_ssid, st_pass);
+    WiFi.softAP(ap_ssid);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println("WifiSet");
+  } else {
+    Serial.println("WifiErr");
+  }
 }
 
--- matrixDisplay/matrixDisplay.ino
+++ matrixDisplay/matrixDisplay.ino
@@ -1,7 +1,7 @@
-// MAX7219 Scrolling Display with DS1307
-// Adapted from the Parola examples
-//
-
+/**
+ * Parola-MAX7219 Serial Message Display with Clock
+ * Dolen Le
+ */
 #include <MD_Parola.h>
 #include <MD_MAX72xx.h>
 #include <Time.h>  
@@ -9,20 +9,9 @@
 #include <DS1307RTC.h>
 #include <SPI.h>
 
-uint8_t degC[] = { 6, 3, 3, 56, 68, 68, 68 };
-uint8_t degF[] = { 6, 3, 3, 124, 20, 20, 4 };
-uint8_t colon[] = {2, 102, 102};
-uint8_t mode = 0;
-uint8_t msgIndex = 0;
-uint8_t msgCount = 1;
-char tm[3] = "AM";
-char monthBuff[10];
-unsigned long timer = 1357041600;
-
 // Parola definitions
-#define	MAX_DEVICES	12
-#define	CS_PIN		10
-MD_Parola P = MD_Parola(CS_PIN, MAX_DEVICES); //using SPI
+#define  MAX_DEVICES 12
+#define CS_PIN    10
 
 #define INITIAL_SPEED 0
 #define MSG_PAUSE 1250
@@ -34,47 +23,55 @@ MD_Parola P = MD_Parola(CS_PIN, MAX_DEVICES); //using SPI
 #define LINE_SEPARATOR '\r'
 #define MSG_SEPARATOR '\n'
 
+enum modes {DISP_DATE, DISP_TIME, DISP_MSG};
+
 textEffect_t  enterEffect = SCROLL_DOWN;
 textEffect_t  exitEffect = SCROLL_DOWN;
-textEffect_t	effectList[] =
+textEffect_t  effectList[] =
 {
-	PRINT,
-	SCAN_HORIZ,
-	SCROLL_LEFT,
-	WIPE,
-	SCROLL_UP_LEFT,
-	SCROLL_UP,
-	OPENING_CURSOR,
-	GROW_UP,
-	SCROLL_UP_RIGHT,
-	BLINDS,
-	CLOSING,
-	GROW_DOWN,
-	SCAN_VERT,
-	SCROLL_DOWN_LEFT,
-	WIPE_CURSOR,
-	DISSOLVE, //15
-	OPENING,
-	CLOSING_CURSOR,
-	SCROLL_DOWN_RIGHT,
-	SCROLL_RIGHT,
-	SCROLL_DOWN,
+  PRINT,
+  SCAN_HORIZ,
+  SCROLL_LEFT,
+  WIPE,
+  SCROLL_UP_LEFT,
+  SCROLL_UP,
+  OPENING_CURSOR,
+  GROW_UP,
+  SCROLL_UP_RIGHT,
+  BLINDS,
+  CLOSING,
+  GROW_DOWN,
+  SCAN_VERT,
+  SCROLL_DOWN_LEFT,
+  WIPE_CURSOR,
+  DISSOLVE, //15
+  OPENING,
+  CLOSING_CURSOR,
+  SCROLL_DOWN_RIGHT,
+  SCROLL_RIGHT,
+  SCROLL_DOWN,
 };
 
+MD_Parola P = MD_Parola(CS_PIN, MAX_DEVICES); //using SPI
+uint8_t degC[] = { 6, 3, 3, 56, 68, 68, 68 };
+uint8_t degF[] = { 6, 3, 3, 124, 20, 20, 4 };
+uint8_t colon[] = {2, 102, 102};
+char ap[3] = "AM";
+unsigned long timer = 1357041600;
+
 //Message buffers
-#define	LINE_MAX 128
+#define	MAX_LENGTH 192
 #define	MAX_LINES 3
-char curMessage[LINE_MAX]; //display buffer
-char newMessage[MAX_LINES][LINE_MAX]; //message buffer
+char curMessage[MAX_LENGTH]; //display buffer
+char newMessage[MAX_LINES][MAX_LENGTH] = {"OK"}; //message buffer
+uint8_t mode = 0;
+uint8_t lineInd = 0;
+uint8_t lineCount = 1;
 
-bool newMessageAvailable = false;
-
-void readSerial(void)
-{
+void readSerial(void) {
   uint8_t arrIndex = 0;
   char nextChar = Serial.read();
-  if(nextChar == SET_RTC_CHAR)
-  {
+  if(nextChar == SET_RTC_CHAR) {
     int dates[6];
     char buf[5];
     buf[4]=0;
@@ -90,11 +87,8 @@ void readSerial(void)
     tmElements_t data = {dates[5],dates[4],dates[3],0,dates[0],dates[1],dates[2]-1970};
     setTime(makeTime(data)-14400); //GMT->EST
     Serial.println(F("Clock Set"));
-  }
-  else
-  {
+  } else {
     while(nextChar >= 0 && nextChar != MSG_SEPARATOR) {
-      Serial.println("read line");
       newMessage[arrIndex][0] = nextChar;
       unsigned int bytes = Serial.readBytesUntil(LINE_SEPARATOR, newMessage[arrIndex]+1, sizeof(newMessage[0])-1);
       newMessage[arrIndex++][bytes+1] = 0;
@@ -103,17 +97,17 @@ void readSerial(void)
         break;
       }
     }
-    msgCount = arrIndex;
-    newMessageAvailable = true;
+    lineCount = arrIndex;
+    Serial.print(lineCount);
+    Serial.println(" lines");
     Serial.println(newMessage[0]);
   }
   while(Serial.available()) Serial.read(); //flush buffer
 }
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
-  Serial.println("[Initializing]");
+  Serial.println(F("[Initializing]"));
   P.begin();
   P.addChar(':', colon);
   P.displayClear();
@@ -122,84 +116,75 @@ void setup()
   P.displayText(curMessage, CENTER, INITIAL_SPEED, CLK_PAUSE, enterEffect, exitEffect);
   
   setSyncProvider(RTC.get);
-  if(timeStatus()!= timeSet) 
+  if(timeStatus()!= timeSet) {
      Serial.println(F("RTC ERR"));
-  else
-     Serial.println(F("RTC OK"));  
+  } else {
+     Serial.println(F("RTC OK"));
+  }  
   SPI.setClockDivider(SPI_CLOCK_DIV2);
 }
 
-void loop() 
-{
-  if(Serial.available())
-  {
+void loop() {
+  if(Serial.available()) {
     readSerial();
   }
-  if (P.displayAnimate()) 
-  {
-      if(millis() - timer > CLK_INTERVAL && msgIndex ==0)
-      {
-        switch(mode)
-        {
-          case 0:
+  if (P.displayAnimate())  {
+      if(millis() - timer > CLK_INTERVAL && lineInd == 0) {
+        switch(mode) {
+          case DISP_DATE:
             P.setPause(CLK_PAUSE);
             P.setSpeed(0);
             P.setTextEffect(SCROLL_DOWN, SCROLL_DOWN);
             sprintf(curMessage, "%s %i, %i", monthStr(month()), day(), year());
             break;
-          case 1:
-          {
+          case DISP_TIME: {
             P.setIntensity(map(analogRead(A7),10,400,0,15)); //ambient brightness
-            sprintf(curMessage, "%i:%02i %s", get12hr(), minute(), tm);
+            sprintf(curMessage, "%i:%02i %s", get12hr(), minute(), ap);
             break;
           }
           default:
-            P.setPause(MSG_PAUSE);
-            if(msgCount == 1) {
-              P.setPause(CLK_INTERVAL);
-            }
-            timer = millis();
-            strcpy(curMessage, newMessage[msgIndex]);
-            if(strlen(curMessage) > 16)
-            {
+            strcpy(curMessage, newMessage[lineInd]);
+            if(strlen(curMessage) > 16) {
               P.setTextEffect(SCROLL_LEFT, SCROLL_LEFT);
               P.setSpeed(20);
               P.setPause(0);
+            } else if(lineCount == 1) {
+              P.setPause(CLK_INTERVAL);
+            } else {
+              P.setPause(MSG_PAUSE);
             }
-            ++msgIndex%=msgCount;
-            newMessageAvailable = false;
+            timer = millis();
+            ++lineInd%=lineCount;
         }
-      ++mode%=3;
-      } else if(msgCount>1)
-      {
-        P.setSpeed(0);
-        P.setTextEffect(enterEffect, exitEffect);
-        P.setPause(MSG_PAUSE);
-        strcpy(curMessage, newMessage[msgIndex]);
-        if(strlen(curMessage) > 16)
-        {
+        ++mode %= DISP_MSG+1;
+      } else if(lineCount>1) {
+        strcpy(curMessage, newMessage[lineInd]);
+        if(strlen(curMessage) > 16) {
           P.setTextEffect(SCROLL_LEFT, SCROLL_LEFT);
           P.setSpeed(20);
           P.setPause(0);
+        } else {
+          P.setSpeed(0);
+          P.setTextEffect(enterEffect, exitEffect);
+          P.setPause(MSG_PAUSE);
         }
-        ++msgIndex%=msgCount;
-        newMessageAvailable = false;
+        ++lineInd%=lineCount;
       }
       P.displayReset();
   }
 }
 
-int get12hr()
-{
+int get12hr() {
   int hr = hour();
-  strcpy(tm,"AM");
-  if(hr == 12) {
-    strcpy(tm,"PM");
+  ap[0] = 'P';
+  if(hr < 12) {
+    if(hr == 0) {
+      hr = 12;
+    }
+    ap[0] = 'A';
   } else if(hr > 12) {
-    strcpy(tm,"PM");
     hr -= 12;
-  } else if(hr == 0)
-    hr = 12; 
+  }
   return hr;
 }
 
