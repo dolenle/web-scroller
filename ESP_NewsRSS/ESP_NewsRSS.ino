71b2ebd5ac1c457b1868d11a4d11c094046eb84e
Dolen Le <le2@cooper.edu>
2016-04-10 03:48:20 -0400

    added NYC subway status, modified server behavior, misc fixes

--- ESP_NewsRSS/ESP_NewsRSS.ino
+++ ESP_NewsRSS/ESP_NewsRSS.ino
@@ -56,7 +56,7 @@ char* effectSelect PROGMEM = "<option value=3>-</option><option value=0>NO_EFFEC
 
 uint8_t numFeeds = 2;
 uint8_t feedSkip[MAX_FEEDS] = {1, 2};
-uint8_t feedLines[MAX_FEEDS] = {10, 1};
+uint8_t feedLines[MAX_FEEDS] = {5, 1};
 char feedURLs[MAX_FEEDS][URL_LENGTH] = {
   "http://www.newslookup.com/rss/wires/newslookup-wires.rss",
   "http://rss.accuweather.com/rss/liveweather_rss.asp?locCode=10003"
@@ -72,6 +72,8 @@ ESP8266WebServer server(80); //http server on port 80
 void setup() {
   Serial.begin(115200);
   Serial1.begin(115200);
+
+  resetDisplay();
   
   WiFi.begin(st_ssid, st_pass);
   WiFi.softAP(ap_ssid);
@@ -79,21 +81,18 @@ void setup() {
   server.on("/", pageHandler);
   server.on("/msg", msgHandler);
   server.on("/feed", feedHandler);
-  server.on("/clock", updateTime);
-  server.on("/reset", resetHandler);
   server.on("/wifi", wifiHandler);
-  server.on("/get", loadMsg);
   server.on("/int", timingHandler);
   server.on("/effect", effectHandler);
   server.on("/ota", HTTP_POST, [](){
-      server.sendContent(redirHeader);
+      server.send(200, "text/plain", (Update.hasError())?"FAIL":"OK");
       ESP.restart();
     }, otaHandler);
   server.begin();
   
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
     server.handleClient();
+    delay(500);
     Serial.print(".");
   }
   Serial.println(WiFi.localIP());
@@ -105,6 +104,7 @@ void loop() {
   unsigned long now=millis();
   if(now-lastUpdate > rss_interval) {
     updateFeeds();
+    loadMTAServiceStatus();
     loadMsg();
     lastUpdate = now;
   } else if(now-lastTimeSet > clock_interval) {
@@ -137,8 +137,8 @@ void fixCharRef(char* reference, char sub) {
   }
 }
 
-//load up to maxLines titles from the feed, skipping at beginning
-int loadFeed(char* url, int skip, int maxLines) {
+//load up to limit titles from the feed, skipping at beginning
+int loadFeed(char* url, int skip, int limit) {
   int src = 0; //source index (from buff)
   int dest = 0; //destination index
   bool copying = false;
@@ -197,7 +197,7 @@ int loadFeed(char* url, int skip, int maxLines) {
             }
           }
         }
-        if(line >= MAX_LINES || count >= maxLines) {
+        if(line >= MAX_LINES || count >= limit) {
           break;
         }
       }
@@ -214,37 +214,44 @@ int loadFeed(char* url, int skip, int maxLines) {
   fixCharRef("&amp;", '&');
   fixCharRef("&quot;", '"');
   fixCharRef("\xe2\x80\x99", '\'');
+  fixCharRef("\xe2\x80\x98", '`');
   fixCharRef("\r\n", '\r');
   return count;
 }
 
 void updateTime() {
   WiFiClient client;
-  while(!client.connect("google.com", 80)) {
-    Serial.println("Time connect error");
-  }
-  client.print("HEAD / HTTP/1.1\r\n\r\n");
-  while(!client.available()) {
-     yield();
-  }
-  while(client.available()) { 
-    if(client.read() == 't' && client.read() == 'e' && client.read() == ':') { //Date:
-      client.read();
-      char dateStr[30];
-      dateStr[29] = 0;
-      client.readBytesUntil('G', dateStr, 29);
-      char* c = dateStr;
-      while(c=strchr(c+1, ' ')) *c=0; //tokenize space
-      c = dateStr+17;
-      while(c=strchr(c+1, ':')) *c=0; //tokenize colon
-      Serial1.write('\005');
-      Serial1.write(dateStr+5, 3); //day
-      Serial1.printf("%02d", (strstr(monthTable, dateStr+8)-monthTable)/4+1); //month
-      Serial1.write(dateStr+11, 15); //year, hr, min, sec
-      client.stop();
+  Serial.println("SetTime...");
+  if(client.connect("google.com", 80)) {
+    client.print("HEAD / HTTP/1.1\r\n\r\n");
+    while(!client.available()) {
+       yield();
     }
+    if(client.available() && client.find("Date: ")) {
+        char dateStr[30];
+        dateStr[29] = 0;
+        client.readBytesUntil('G', dateStr, 29);
+        char* c = dateStr;
+        while(c=strchr(c+1, ' ')) *c=0; //tokenize space
+        c = dateStr+17;
+        while(c=strchr(c+1, ':')) *c=0; //tokenize colon
+        Serial1.write('\005');
+        Serial1.write(dateStr+5, 3); //day
+        Serial1.printf("%02d", (strstr(monthTable, dateStr+8)-monthTable)/4+1); //month
+        Serial1.write(dateStr+11, 15); //year, hr, min, sec
+        client.stop();
+        Serial.println("Done");
+    }
+  } else {
+    Serial.println("Error");
   }
-  server.sendContent(redirHeader);
+}
+
+//Reset the display driver
+void resetDisplay() {
+  pinMode(0, OUTPUT);
+  delay(10);
+  pinMode(0, INPUT);
 }
 
 void loadMsg() {
@@ -252,7 +259,7 @@ void loadMsg() {
   HTTPClient http;
   http.begin("http:\/\/dolenle.com/led/getMsg.php?key=URl33tHAXORlol4lyef");
   int httpCode = http.GET();
-  if(httpCode == HTTP_CODE_OK && line < MAX_LINES-1) {
+  if(httpCode == HTTP_CODE_OK && line < MAX_LINES) {
     String payload = http.getString();
     Serial.println(payload);
     payload.toCharArray(headlines[line++], sizeof(headlines[0]));
@@ -261,12 +268,11 @@ void loadMsg() {
   }
   http.end();
   fixCharRef("\r\n", '\r');
-  server.sendContent(redirHeader);
 }
 
 int updateFeeds() {
   line = 0;
-  for(int i = 0; i < numFeeds && line < MAX_LINES-1; i++) {
+  for(int i = 0; i < numFeeds && line < MAX_LINES; i++) {
     loadFeed(feedURLs[i], feedSkip[i], feedLines[i]);
   }
   Serial.print(line);
@@ -280,9 +286,66 @@ int updateFeeds() {
   return line;
 }
 
+//MTA website sucks
+void loadMTAServiceStatus() {
+  WiFiClient client;
+  Serial.print("MTAService...");
+  bool secondLine = false;
+  if(line < MAX_LINES-2 && client.connect("web.mta.info", 80)) {
+    //create http request
+    client.print("GET /status/serviceStatus.txt HTTP/1.1\r\nHost: web.mta.info\r\nConnection: close\r\n\r\n");
+    while (!client.available()) {
+      yield();
+    }
+    char* intro = "Subway Status";
+    int index = strlen(intro); //next free index
+    strcpy(headlines[line], intro);
+    headlines[line][index++] = '\r'; //line separator
+    while(client.available() && client.findUntil("<name>", "</subway>")) {
+      String txt = client.readStringUntil('<');
+      headlines[line][index++] = '(';
+      txt.toCharArray(headlines[line]+index, sizeof(headlines)-index);
+      index += txt.length();
+      headlines[line][index++] = ')';
+      strcpy(headlines[line]+index, " - ");
+      index += 3;
+      client.find("<status>");
+      txt = client.readStringUntil('<');
+      txt.toLowerCase();
+      txt.toCharArray(headlines[line]+index, sizeof(headlines)-index);
+      index += txt.length();
+      strcpy(headlines[line]+index, "    ");
+      index += 4;
+      if(index > 120 && !secondLine) { //nearing end
+        line++;
+        index = 0;
+        secondLine = true;
+      }
+    }
+    line++;
+    Serial.println("Done");
+    client.stop();
+  } else {
+    Serial.println("Error"); //connect failed or not enough buffer space
+  }
+}
+
 //Main server page
 void pageHandler() {
-  String content = "<html><head><title>web-scroller</title><style>body{font-family:Helvetica,Arial,Sans-Serif;}</style></head><body><h1>ESP8266 web-scroller</h1><h4><a href='/clock'>Update Clock</a> | <a href='/reset'>Reset Display</a> | <a href='/get'>Get Message</a></h4><form action='/msg' method='POST'>Add Message:<input name='msg' size=90> <input type='submit'></form>Animation:<form action='/effect'>Enter: <select name='enter'>";
+  if(server.hasArg("mta")) {
+    loadMTAServiceStatus();
+  } else if(server.hasArg("clk")) {
+    updateTime();
+  } else if(server.hasArg("rst")) {
+    resetDisplay();
+  } else if(server.hasArg("get")) {
+    loadMsg();
+  }
+  if(server.args()) {
+    server.sendContent(redirHeader);
+    return;
+  }
+  String content = "<html><head><title>web-scroller</title><style>body{font-family:Helvetica,Arial,Sans-Serif;}</style></head><body><h1>ESP8266 web-scroller</h1><h4><a href='/?clk=1'>Update Clock</a> | <a href='/?rst=1'>Reset Display</a> | <a href='/?get=1'>Get Message</a> | <a href='/?mta=1'>Get Subway Status</a></h4><form action='/msg' method='POST'>Temporary Message:<input name='msg' size=90> <input type='submit'></form>Animation:<form action='/effect'>Enter: <select name='enter'>";
   content += effectSelect;
   content += " Exit: <select name='exit'>";
   content += effectSelect;
@@ -299,14 +362,14 @@ void pageHandler() {
     content += feedURLs[i];
     content += "'> Skip:<input name='skip"+String(i)+"' size=3 value='"+String(feedSkip[i])+"'> Max:<input name='max"+String(i)+"' size=3 value='"+String(feedLines[i])+"'><br>";
   }
-  content += "<input type='submit'></form>WiFi Settings:<form action='/wifi' method='POST'>SSID:<input name='st_ssid' value='"+String(st_ssid)+"'> PASS:<input type='password' name='st_pass' placeholder='*****'> AP SSID:<input name='ap_ssid' value='"+String(ap_ssid)+"'> <input type='submit'></form>Intervals:<form action='/int'>Feed:<input name='rss' size=9 value="+String(rss_interval)+"> Clock:<input name='time' size=9 value="+String(clock_interval)+"> Display:<input name='disp' size=9 value="+String(disp_interval)+"><input type='submit'></form><form method='POST' action='/ota' enctype='multipart/form-data'>Firmware: <input type='file' name='update'><input type='submit'></form></body></html>";
+  content += "<input type='submit'></form>WiFi Settings:<form action='/wifi' method='POST'>SSID:<input name='st_ssid' value='"+String(st_ssid)+"'> PASS:<input type='password' name='st_pass' placeholder='*****'> AP SSID:<input name='ap_ssid' value='"+String(ap_ssid)+"'> <input type='submit'></form>Intervals (ms):<form action='/int'>Feed:<input name='rss' size=9 value="+String(rss_interval)+"> Clock:<input name='time' size=9 value="+String(clock_interval)+"> Display:<input name='disp' size=9 value="+String(disp_interval)+"><input type='submit'></form><form method='POST' action='/ota' enctype='multipart/form-data'>Firmware: <input type='file' name='update'><input type='submit'></form></body></html>";
   server.send(200, "text/html", content);
 }
 
 //Manually add a message
 void msgHandler() {
   if(server.hasArg("msg")) {
-    if(line < MAX_LINES-1) {
+    if(line < MAX_LINES) {
       server.arg("msg").toCharArray(headlines[line++], sizeof(headlines[0]));
     }
     Serial1.println(server.arg("msg"));
@@ -336,19 +399,11 @@ void feedHandler() {
   server.sendContent(redirHeader);
 }
 
-//Reset the display driver
-void resetHandler() {
-  pinMode(0, OUTPUT);
-  delay(10);
-  pinMode(0, INPUT);
-  server.sendContent(redirHeader);
-}
-
 //Update WiFi Settings
 void wifiHandler() {
   if(server.hasArg("st_ssid") && server.hasArg("st_pass") && server.hasArg("ap_ssid")) {
-    server.arg("st_ssid").toCharArray(st_ssid, sizeof(st_ssid));
-    if(server.arg("st_pass").length()) {
+    if(server.arg("st_pass").length()) { //update only if password entered
+      server.arg("st_ssid").toCharArray(st_ssid, sizeof(st_ssid));
       server.arg("st_pass").toCharArray(st_pass, sizeof(st_pass));
     }
     server.arg("ap_ssid").toCharArray(ap_ssid, sizeof(ap_ssid));
@@ -356,6 +411,7 @@ void wifiHandler() {
     WiFi.begin(st_ssid, st_pass);
     WiFi.softAP(ap_ssid);
     while (WiFi.status() != WL_CONNECTED) {
+      server.handleClient();
       delay(500);
       Serial.print(".");
     }
@@ -375,6 +431,7 @@ void timingHandler() {
   server.sendContent(redirHeader);
 }
 
+//Change text animation
 void effectHandler() {
   if(server.hasArg("enter") && server.hasArg("exit")) {
     Serial1.write(1);
@@ -386,20 +443,21 @@ void effectHandler() {
   server.sendContent(redirHeader);
 }
 
+//Firmware update
 void otaHandler() {
   HTTPUpload& upload = server.upload();
-  if(upload.status == UPLOAD_FILE_START){
+  if(upload.status == UPLOAD_FILE_START) {
     Serial.setDebugOutput(true);
     Serial.printf("Update: %s\n", upload.filename.c_str());
     uint32_t maxSketchSpace = (ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000;
     if(!Update.begin(maxSketchSpace)){//start with max available size
       Update.printError(Serial);
     }
-  } else if(upload.status == UPLOAD_FILE_WRITE){
+  } else if(upload.status == UPLOAD_FILE_WRITE) {
     if(Update.write(upload.buf, upload.currentSize) != upload.currentSize){
       Update.printError(Serial);
     }
-  } else if(upload.status == UPLOAD_FILE_END){
+  } else if(upload.status == UPLOAD_FILE_END) {
     if(Update.end(true)){ //true to set the size to the current progress
       Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
     } else {
@@ -409,4 +467,5 @@ void otaHandler() {
   }
   yield();
 }
+
 
